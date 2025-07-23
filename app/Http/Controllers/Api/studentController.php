399deeb436487eef9a9ed6454e427105e8463b42
Rody Huancas <rodyhuancas.04@gmail.com>
2025-07-23 16:49:56 -0500

    Feat: Add methods to display, update, partially update, and delete students in the API controller.

--- app/Http/Controllers/Api/studentController.php
+++ app/Http/Controllers/Api/studentController.php
@@ -69,4 +69,139 @@ class studentController extends Controller
 
         return response()->json($data, 201);
     }
+
+    public function show($id)
+    {
+        $student = Student::find($id);
+
+        if (!$student) {
+            $data = [
+                "message" => "No se encontró el estudiante",
+                "status"  => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $data = [
+            "student" => $student,
+            "status"  => 200
+        ];
+
+        return response()->json($data, 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $student = Student::find($id);
+
+        if (!$student) {
+            $data = [
+                "message" => "No se encontró el estudiante",
+                "status"  => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name'     => 'required',
+            'email'    => 'required|email',
+            'phone'    => 'required',
+            'language' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            $data = [
+                "message" => "Error en la validación de los datos",
+                "errors"  => $validator->errors(),
+                "status"  => 400
+            ];
+            return response()->json($data, 400);
+        }
+
+        $student->name     = $request->name;
+        $student->email    = $request->email;
+        $student->phone    = $request->phone;
+        $student->language = $request->language;
+
+        $data = [
+            "message" => "Estudiante actualizado",
+            "student" => $student,
+            "status"  => 200
+        ];
+
+        return response()->json($data, 200);
+    }
+
+    public function updatePartial(Request $request, $id) {
+        $student = Student::find($id);
+
+        if (!$student) {
+            $data = [
+                "message" => "No se encontró el estudiante",
+                "status"  => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name'     => 'max:255',
+            'email'    => 'email|unique:student',
+            'phone'    => 'digits:10',
+            'language' => 'in:English,Spanish,French'
+        ]);
+
+        if ($validator->fails()) {
+            $data = [
+                "message" => "Error en la validación de los datos",
+                "errors"  => $validator->errors(),
+                "status"  => 400
+            ];
+            return response()->json($data, 400);
+        }
+
+        if ($request->has('name')) {
+            $student->name = $request->name;
+        }
+        if ($request->has('email')) {
+            $student->email = $request->email;
+        }
+        if ($request->has('phone')) {
+            $student->phone = $request->phone;
+        }
+        if ($request->has('language')) {
+            $student->language = $request->language;
+        }
+
+        $student->save();
+
+        $data = [
+            "message" => "Estudiante actualizado",
+            "student" => $student,
+            "status"  => 200
+        ];
+
+        return response()->json($data, 200);
+    }
+
+    public function destroy($id)
+    {
+        $student = Student::find($id);
+
+        if (!$student) {
+            $data = [
+                "message" => "No se encontró el estudiante",
+                "status"  => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $student->delete();
+
+        $data = [
+            "message" => "Estudiante eliminado correctamente",
+            "status"  => 200
+        ];
+
+        return response()->json($data, 200);
+    }
 }
